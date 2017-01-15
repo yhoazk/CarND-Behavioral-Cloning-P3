c2082f21c891ccf91a978dff6e93db27c7796e65
porko <yhoazk@gmail.com>
2017-01-15 01:56:19 -0600

    Change activators for layers and also include reshape of image in drive.py file

--- drive.py
+++ drive.py
@@ -37,6 +37,8 @@ def telemetry(sid, data):
     imgString = data["image"]
     image = Image.open(BytesIO(base64.b64decode(imgString)))
     image_array = np.asarray(image)
+    # Crop image
+    image_array = image_array[50:140]
     transformed_image_array = image_array[None, :, :, :]
     # This model currently assumes that the features of the model are just the images. Feel free to change this.
     steering_angle = float(model.predict(transformed_image_array, batch_size=1))
