457961ec165adaf9ee28ab3678e6e18a51c6cd37
porko <yhoazk@gmail.com>
2017-01-28 02:27:13 -0600

    Notebook updated

--- drive.py
+++ drive.py
@@ -52,7 +52,7 @@ def telemetry(sid, data):
     # The driving model currently just outputs a constant throttle. Feel free to edit this.
 #    throttle = '0.01' if throttle <= '0.0' else throttle
 #    throttle = str(int(throttle)*1.1) if (int(throttle) < 0.2) else throttle
-    throttle = 0.1
+    throttle = 0.2
     #.imwrite(str(steering_angle)+".png", image_array)
     if save_imgs:
         image.save(str(int(time.time()*100))+"_"+ str(steering_angle)+".png", "PNG")
