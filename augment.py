77b6ac459279401b7a3b48a671e7989e623ed767
porko <yhoazk@gmail.com>
2017-01-28 02:06:40 -0600

    Ahother model generated to verify :D

--- augment.py
+++ augment.py
@@ -7,15 +7,13 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import random
 import numpy as np
-
-import matplotlib
-from matplotlib.patches import Rectangle
-from matplotlib.collections import PatchCollection
+import csv
 
 IMG = "./center_2016_12_01_13_31_12_937.jpg"
 LBL = -0.55
 
-def shift_lr_random(image, label =None):
+
+def shift_lr_random(image, label=None):
     shift = random.randint(-10, 10)
     shift_lbl = (0.005 * shift) + label
     M = np.float32([[1, 0, shift], [0, 1, 0]])
@@ -28,55 +26,96 @@ def shift_ul_random(image, label):
     M = np.float32([[1, 0, 0], [0, 1, shift]])
     return (cv2.warpAffine(image, M, (image.shape[1], image.shape[0])), shift_lbl)
 
+
 def mirror(image, label):
-    mirr_img = cv2.flip(image,1)
-    label = label * -1. ## add little offset so the image is not exactly the same
-    return (np.add(mirr_img, random.uniform (-0.5, 0.5) ), label)
+    mirr_img = cv2.flip(image, 1)
+    label = label * -1.  ## add little offset so the image is not exactly the same
+    return (np.add(mirr_img, random.uniform(-0.5, 0.5)), label)
+
 
 def add_squares(image, label):
-    n = random.randint(5, 15) # number of rect
+    n = random.randint(5, 15)  # number of rect
 
     for i in range(n):
         x = random.randint(10, 320 - 10)  # x coorfinate
         y = random.randint(10, 90 - 10)  # y coordinate of rect
         size = random.randint(5, 20)  # size of rectangle
-        img = cv2.rectangle(image,(x,y), (x+size, y+size), (-.5,-.5,-.5), -1)
+        img = cv2.rectangle(image, (x, y), (x + size, y + size), (-.5, -.5, -.5), -1)
 
     return (img, label)
-    #p.set_array(np.zeros())
+    # p.set_array(np.zeros())
 
-
-def plot_imgArr(img_arr, label=None, predict=None, gray=False):
-    f, arr = plt.subplots(5,4)
-    print(img_arr[0].shape)
-    for n, subplt in enumerate(arr.reshape(-1)):
-        if gray:
-            subplt.imshow(img_arr[n],  cmap='gray')
-        else:
-            subplt.imshow(img_arr[n])
-        subplt.axis('off')
-        if label is not None and predict is None:
-            subplt.set_title("st: "+str(label[n]))
-        elif label is not None and predict is not None:
-            subplt.set_title("st:"+str(label[n]) + "p:"+str(predict[n]))
-    plt.show()
 
 def augment(image, label):
-    if random.choice([True, True, True,True, False]):
+    if random.choice([True, True, True, True, False]):
 
-        ops = random.randint(1,4)
-        fncs = [mirror, add_squares,  shift_ul_random, shift_lr_random]
+        ops = random.randint(1, 4)
+        fncs = [mirror, add_squares, shift_ul_random, shift_lr_random]
         for _ in range(ops):
             fnc = np.random.choice(fncs, replace=False)
-            image, label = fnc(image,label)
+            image, label = fnc(image, label)
 
     return (image, label)
 
 
-
-
 def preprocessrgb2gray(image_path, dims):
     return np.multiply(np.mean(np.asarray(Image.open(image_path).crop(dims)), -1), 1 / 255)
+
+
+def augmentBin(X, y, prob):
+    for i, (img, lbl) in enumerate(zip(X, y)):
+        if random.random() >= prob:
+            _X, _y = augment(img, lbl)
+            y.append(_y)
+            X.append(_X)
+
+    return (X, y)
+
+
+def get_dataset(imgdir, csvf):
+    """
+    imgdir is the path to IMG dir
+    csvf is the path for csv file
+    """
+    imgs = []
+    labels = []
+    csv_r = csv.reader(open(csvf, 'r'))
+    for ln in csv_r:
+        # imgs.append(preprocessrgb2gray(imgdir+ln[0], (0,50,320,150)))
+        # get only the path
+        imgs.append(imgdir + ln[0])
+        labels.append(float(ln[3]))
+
+    return (imgs, labels)
+
+
+def analyze_dataset(angle_list):
+    n, bins, _ = plt.hist(angle_list, facecolor='green', bins=21)
+    plt.xlabel('Steering angle')
+    plt.ylabel('Number of images in the range')
+    plt.grid(True)
+    plt.show()
+
+    # convert n, which is in float to int. n indicates the number of elements in the bin
+    n = list(map(int, n))
+    return (n, bins)
+
+
+def plot_imgArr(img_arr, label=None, predict=None, gray=False, n=2):
+    f, arr = plt.subplots(n, 2)
+    print(img_arr[0].shape)
+    for n, subplt in enumerate(arr.reshape(-1)):
+        if gray:
+            subplt.imshow(img_arr[n], cmap='gray')
+        else:
+            subplt.imshow(img_arr[n])
+        subplt.axis('off')
+        if label is not None and predict is None:
+            subplt.set_title("st: " + str(label[n]))
+        elif label is not None and predict is not None:
+            subplt.set_title("st:" + str(label[n]) + "p:" + str(predict[n]))
+    plt.show()
+
 
 """
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -1,1 +1,189 @@
 #!/usr/bin/env python
+
+
+from augment import *
+import random
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+from keras.models import Sequential
+from keras.layers import Convolution2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
+from keras.optimizers import Adam
+import gc
+from keras.callbacks import ModelCheckpoint
+from keras import backend as K
+#define constants
+
+
+#SIMULATOR_DIR = "/home/porko/Descargas/simulator-linux/"
+SIMULATOR_DIR ="/home/porko/Descargas/simulator-linux/"
+#SIMULATOR_DIR = "../simulator-linux/"
+IMG_DIR       = SIMULATOR_DIR# + "IMG/"
+LABEL_CSV     = SIMULATOR_DIR + "driving_log.csv"
+
+PARK_REC_IMG  = "/home/porko/Descargas/simulator-linux/prk/"#SIMULATOR_DIR + "prk/"#IMG/"
+PARK_REC_CSV  = PARK_REC_IMG + "/driving_log.csv"
+
+LEFT_REC_IMG  = "/home/porko/Descargas/simulator-linux/lft/"#SIMULATOR_DIR + "lft/"#IMG/"
+LEFT_REC_CSV  = LEFT_REC_IMG + "/driving_log.csv"
+
+RIGHT_REC_IMG  = "/home/porko/Descargas/simulator-linux/" + "rgt/"#IMG/"
+RIGHT_REC_CSV  = RIGHT_REC_IMG + "/driving_log.csv"
+
+BOTTOM_MARGIN = 50
+TOP_MARGIN = 140
+
+
+# Get data sets
+
+
+
+X_all,y_all = get_dataset(IMG_DIR, LABEL_CSV )
+n_all, bins_all = analyze_dataset(y_all)
+
+X_park,y_park = get_dataset(PARK_REC_IMG, PARK_REC_CSV )
+# Make all the samples more to the right
+for n,y in enumerate(y_park):
+    y_park[n] = (-1.*abs(y)) + (random.randint(1, 10) / -100)
+
+#n_park, bins_park = analyze_dataset(y_park)
+
+
+
+X_lft,y_lft = get_dataset(LEFT_REC_IMG, LEFT_REC_CSV )
+# Make all the samples more to the right
+for n,y in enumerate(y_lft):
+    y_lft[n] = (-1.*abs(y)) + (random.randint(1, 10) / -100)
+
+#n_lft, bins_lft = analyze_dataset(y_lft)
+
+X_rgt, y_rgt = get_dataset(RIGHT_REC_IMG, RIGHT_REC_CSV)
+# Make all the samples more to the right
+for n, y in enumerate(y_rgt):
+    y_rgt[n] = abs(y) + (random.randint(1, 10) / 100)
+
+#n_rgt, bins_rgt = analyze_dataset(y_rgt)
+
+# create list for data
+X = [[] for _ in range(len(n_all))]
+# create list for labels]
+y = [[] for _ in range(len(n_all))]
+random.seed(1010)
+# Transformtations to augment the data set
+X_all = X_lft + X_park + 2 * X_rgt + X_all
+y_all = y_lft + y_park + 2 * y_rgt + y_all
+for i, (lbl, img) in enumerate(zip(y_all, X_all)):
+    if -0.1 <= lbl <= 0.038 and random.random() >= 0.4:
+        y_all.pop(i)
+        X_all.pop(i)
+
+        # Separate the images in bins for augmentation
+for label, abs_path in zip(y_all, X_all):
+    # Increase the data set.
+    for b in range(len(n_all) - 1):
+        if bins_all[b] < label <= bins_all[b + 1]:
+            X[b].append(preprocessrgb2gray(abs_path, (0, BOTTOM_MARGIN, 320, TOP_MARGIN)))
+            y[b].append(label)
+
+for x in range(len(n_all)):
+    print(len(X[x]))
+
+# remove 40% of images with steering close to zero
+for i, (lbl, img) in enumerate(zip(y[10], X[10])):
+    if random.random() >= 0.2:
+        y[10].pop(i)
+        X[10].pop(i)
+
+        # Add more data by augmentation in each bin, first try to balance
+X[8], y[8] = augmentBin(X[8], y[8], 0.5)
+
+X[11], y[11] = augmentBin(X[11], y[11], 0.5)
+
+X[13], y[13] = augmentBin(X[13], y[13], 0.5)
+X[7], y[7] = augmentBin(X[7], y[7], 0.5)
+X[14], y[14] = augmentBin(X[14], y[14], 0.5)
+X[6], y[6] = augmentBin(X[6], y[6], 0.5)
+
+X[15], y[15] = augmentBin(X[15], y[15], 0.5)
+X[5], y[5] = augmentBin(X[5], y[5], 0.5)
+X[4], y[4] = augmentBin(X[4], y[4], 0.5)
+
+X[17], y[17] = augmentBin(X[17], y[17], 0.5)
+X[18], y[18] = augmentBin(X[18], y[18], 0.5)
+
+# flat list of lists
+
+X = sum(X, [])
+y = sum(y, [])
+gc.collect()
+print(len(X))
+print(len(y))
+
+### Re print the graph
+
+n, bins, _ = plt.hist(y, facecolor='green', bins=20)
+print("New negative images count: " + str(sum(n[0:9])))
+print("New postivie images count: " + str(sum(n[11:19])))
+
+plt.xlabel('Steering angle')
+plt.ylabel('Number of images in the range')
+plt.grid(True)
+plt.show()
+
+
+X = np.asarray(X)
+X = X.reshape((X.shape[0],X.shape[1],X.shape[2],1))
+y = np.asarray(y).astype('float32')
+y = y.reshape((y.shape[0],1))
+
+X,y = shuffle(X,y,random_state=1)
+X, X_val, y, y_val = train_test_split(X, y, random_state=0, test_size=0.2)
+
+
+
+# Instantiate a Sequential model
+model = Sequential()
+# Add first convolution layer
+
+#
+
+model.add(Convolution2D(1,1,1, border_mode='same', input_shape=(X.shape[1],X.shape[2],X.shape[3]), dim_ordering='tf'))
+# shape ()
+model.add(Convolution2D(24, 8, 8, border_mode='valid', activation='relu'))
+model.add(MaxPooling2D((2,2),(2,2),'valid'))
+model.add(Dropout(0.2))
+
+model.add(Convolution2D(36, 5, 5, border_mode='valid', activation='relu'))
+model.add(MaxPooling2D((2,2),(2,2),'valid'))
+model.add(Dropout(0.2))
+
+model.add(Convolution2D(48, 3, 3, border_mode='valid', activation='relu'))
+model.add(MaxPooling2D((2,2),(2,2),'valid'))
+model.add(Dropout(0.2))
+
+model.add(Convolution2D(64, 3, 3, border_mode='valid', activation='relu'))
+model.add(MaxPooling2D((2,2),(2,2),'valid'))
+model.add(Dropout(0.2))
+
+
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dense(512,activation='relu'))
+model.add(Dense(256,activation='relu'))
+model.add(Dense(128,activation='relu'))
+model.add(Dense(64,activation='tanh'))
+model.add(Dense(32,activation='tanh'))
+model.add(Dense(1))
+model.summary()
+
+adam_opt = Adam(lr=0.0001 )
+model.compile(loss='mean_squared_error',optimizer=adam_opt)
+checkpointer = ModelCheckpoint(filepath="./weights-{epoch:02d}.h5", verbose=1, save_best_only=False)
+log = model.fit(X, y,batch_size=40, nb_epoch=25, verbose=1, validation_data=(X_val, y_val), callbacks=[checkpointer])
+
+open("model.json", "w").write(model.to_json())
+model.save_weights("model.h5")
+gc.collect()
+K.clear_session()
+
+print("--- Done ---")
+
